618b83117086954fca533eba7ba518c3a08bc367
David Cornu <davidjcornu@gmail.com>
2012-11-23 16:01:06 -0500

    Add query string support for additional keys

--- index.js
+++ index.js
@@ -6,22 +6,33 @@ exports.extend = function(app){
   app._paths = {};
 
   app.addPath = function(pathName, pathString){
-    var buf = [];
+    var keyList = [];
+    var buf     = [];
 
     pathString.split('/').forEach(function(part){
-        if (keyRxp.test(part)) {
-          buf.push('encodeURIComponent(keys["' + part.match(keyRxp)[1] + '"])');
-        } else {
-          return buf.push('"' + part + '"');
-        }
+      if (keyRxp.test(part)) {
+        var key = part.match(keyRxp)[1]
+        buf.push('encodeURIComponent(keys["' + key + '"])');
+        keyList.push(key);
+      } else {
+        buf.push('"' + part + '"');
+      }
     });
 
-    var body = [
-      '  keys = keys || {};'
-      '  return ' + buf.join(' + "/" + ') + ';'
-    ].join('\n');
+    var template = new Function('keys', 'return ' + buf.join(' + "/" + ') + ';');
 
-    app._paths[pathName] = new Function('keys', body);
+    app._paths[pathName] = function(keys){
+      keys = keys || {};
+
+      var queries = [];
+      Object.keys(keys).forEach(function(k){
+        if (keyList.indexOf(k) >= 0) return;
+        queries.push(encodeURIComponent(k) + '=' + encodeURIComponent(keys[k]));
+      });
+
+      var queryString = queries.length > 0 ? '?' + queries.join('&') : null;
+      return queryString ? template(keys) + queryString : template(keys);
+    }
   }
 
   methods.concat('all').forEach(function(method){
@@ -40,10 +51,12 @@ exports.extend = function(app){
     }
   });
 
-  app.path = function(){
-    var args = Array.prototype.slice.call(arguments);
-    if (args.length == 0) return;
-    var pathName = args.shift();
+  app.path = function(pathName, keys){
+    keys = keys || {};
+    if (!app._paths[pathName]) throw 'Path ' + pathName + ' is not defined.';
+    return app._paths[pathName](keys);
   }
+
+  app.locals({p: path});
 
 }
